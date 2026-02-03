8a1ca1df3648036e3a345b9630d5622df797e603
Darwin Roa <darwinmichaelroamora@gmail.com>
2026-02-02 20:47:37 -0500

    feat(breadcrumbs): Agrega shortcode para los breadcrumbs

--- functions.php
+++ functions.php
@@ -15,3 +15,51 @@ function eventos_y_matrimonios_enqueue_styles() {
 /*
  * Your code goes below
  */
+
+function mdw_eventos_matrimonios_scripts() {
+    wp_enqueue_style('mdw-styles', get_stylesheet_directory_uri() . '/assets/css/style.css');
+}
+add_action( 'wp_enqueue_scripts', 'mdw_eventos_matrimonios_scripts' );
+
+function mdw_breadcrumbs() {
+    // Configuraciones
+    $separator  = ' / ';
+    $home_title = 'Inicio';
+    
+    // Si es la página de inicio, no mostrar nada
+    if ( is_front_page() ) {
+        return '';
+    }
+
+    global $post;
+    $output = '<nav class="mdw__breadcrumbs">';
+    
+    // Enlace al inicio
+    $output .= '<a href="' . get_home_url() . '">' . $home_title . '</a>' . $separator;
+
+    if ( is_category() || is_single() ) {
+        // Mostrar categorías si es un post
+        $cats = get_the_category( $post->ID );
+        if( !empty($cats) ){
+            $output .= '<a href="' . get_category_link( $cats[0]->term_id ) . '">' . $cats[0]->name . '</a>' . $separator;
+        }
+        if ( is_single() ) {
+            $output .= '<span>' . get_the_title() . '</span>';
+        }
+    } elseif ( is_page() ) {
+        // Manejar páginas padre/hijo
+        if ( $post->post_parent ) {
+            $anc = get_post_ancestors( $post->ID );
+            $anc = array_reverse( $anc );
+            foreach ( $anc as $ancestor ) {
+                $output .= '<a href="' . get_permalink( $ancestor ) . '">' . get_the_title( $ancestor ) . '</a>' . $separator;
+            }
+        }
+        $output .= '<span>' . get_the_title() . '</span>';
+    }
+
+    $output .= '</nav>';
+    
+    return $output;
+}
+add_shortcode( 'breadcrumbs', 'mdw_breadcrumbs' );
