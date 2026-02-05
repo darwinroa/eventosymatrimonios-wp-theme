470544a2bfd1db6d1726aa2296beb97aab256323
Darwin Roa <darwinmichaelroamora@gmail.com>
2026-02-05 09:45:14 -0500

    feat(single anuncios): Convierte las imagenes en un carusel usando swiper slider

--- hivepress/listing/view/page/listing-images.php
+++ hivepress/listing/view/page/listing-images.php
@@ -9,22 +9,34 @@ if ( $listing->get_images__id() ) :
 		$image_urls = $listing->get_images__url( 'large' );
 	endif;
 	?>
-	<div class="hp-listing__images" data-component="carousel-slider" data-aspect-ratio="<?php echo esc_attr( hivepress()->asset->get_aspect_ratio( 'landscape_large' ) ); ?>">
-		<?php
-		foreach ( $listing->get_images() as $image_index => $image ) :
-			$image_url = hivepress()->helper->get_array_value( $image_urls, $image_index, '' );
 
-			if ( strpos( $image->get_mime_type(), 'video' ) === 0 ) :
-				?>
-				<video data-zoom="<?php echo esc_url( $image_url ); ?>" controls>
-					<source src="<?php echo esc_url( $image->get_url() ); ?>#t=0.001" type="<?php echo esc_attr( $image->get_mime_type() ); ?>">
-				</video>
-			<?php else : ?>
-				<img src="<?php echo esc_url( $image->get_url( 'hp_landscape_large' ) ); ?>" data-zoom="<?php echo esc_url( $image_url ); ?>" alt="<?php echo esc_attr( $listing->get_title() ); ?>" loading="lazy">
-				<?php
-			endif;
-		endforeach;
-		?>
+    <div class="swiper hp-listing-slider">
+		<div class="swiper-wrapper">
+			<?php
+			foreach ( $listing->get_images() as $image_index => $image ) :
+				$image_url = hivepress()->helper->get_array_value( $image_urls, $image_index, '' );
+                ?>
+                
+                <div class="swiper-slide">
+                    <?php if ( strpos( $image->get_mime_type(), 'video' ) === 0 ) : ?>
+                        <video controls>
+                            <source src="<?php echo esc_url( $image->get_url() ); ?>" type="<?php echo esc_attr( $image->get_mime_type() ); ?>">
+                        </video>
+                    <?php else : ?>
+                        <img src="<?php echo esc_url( $image->get_url( 'hp_landscape_large' ) ); ?>" 
+                             data-zoom="<?php echo esc_url( $image_url ); ?>" 
+                             alt="<?php echo esc_attr( $listing->get_title() ); ?>" 
+                             loading="lazy"
+                             style="width: 100%; height: 100%; object-fit: cover;"> <?php endif; ?>
+                </div>
+
+			<?php endforeach; ?>
+		</div>
+
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+
+        <div class="swiper-pagination"></div>
 	</div>
-	<?php
-endif;
+
+<?php endif; ?>
